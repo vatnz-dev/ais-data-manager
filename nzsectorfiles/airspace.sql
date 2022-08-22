b506e31625c2b780e1f4fc942d0087015b39d058
Tom <13634684+dirtyformal@users.noreply.github.com>
2022-08-23 10:58:26 +1200

    feat: Re-add Domestic TMAs, and NZZO TMAs/Twrs

--- nzsectorfiles/airspace.sql
+++ nzsectorfiles/airspace.sql
@@ -35,7 +35,7 @@ CREATE TABLE `airspace` (
   `ExportID` enum('NZZC','VSPY','VSYS','NZZCVSYS','NZZO','NZZOVSYS','NZZCNZZO') NOT NULL DEFAULT 'NZZC',
   PRIMARY KEY (`id`),
   UNIQUE KEY `ident` (`ident`)
-) ENGINE=InnoDB AUTO_INCREMENT=10333 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=10348 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
