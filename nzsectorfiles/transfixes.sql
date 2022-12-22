2bf91805ad7d604a99cd13716ef5a917b0f06d55
Tom <13634684+dirtyformal@users.noreply.github.com>
2022-12-22 15:10:27 +1300

    fix: Small procedure errors

--- nzsectorfiles/transfixes.sql
+++ nzsectorfiles/transfixes.sql
@@ -26,7 +26,7 @@ CREATE TABLE `transfixes` (
   `Procedures_id` int(11) NOT NULL,
   PRIMARY KEY (`id`,`Procedures_id`),
   KEY `fk_ProcFixes_Procedures1_idx` (`Procedures_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18471 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18485 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
