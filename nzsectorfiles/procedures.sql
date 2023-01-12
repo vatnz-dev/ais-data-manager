c904e93ca89e549eecbf6a59e1cf3cc3592e43eb
Tom <13634684+dirtyformal@users.noreply.github.com>
2023-01-12 16:17:36 +1300

    AIP Procedure Changes added

--- nzsectorfiles/procedures.sql
+++ nzsectorfiles/procedures.sql
@@ -34,7 +34,7 @@ CREATE TABLE `procedures` (
   `sctfilename` varchar(45) NOT NULL,
   `colour` varchar(45) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4122 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4124 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
