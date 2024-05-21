43a67aafbdd23100299f7d441f7aea043a0ee01d
Richard Bywater <richard.bywater@vatnz.net>
2024-05-21 23:03:39 +1200

    Various updates

--- nzsectorfiles/magvar.sql
+++ nzsectorfiles/magvar.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: nzsectorfiles
 -- ------------------------------------------------------
--- Server version	10.4.22-MariaDB
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,7 +22,7 @@ CREATE TABLE `magvar` (
   `ICAO` varchar(4) NOT NULL,
   `magvar` double NOT NULL,
   UNIQUE KEY `ICAO` (`ICAO`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
