46d7dedd5b00ee0e0e69561e2d406ab86b7cf1c1
rb-vatnz <125234863+rb-vatnz@users.noreply.github.com>
2024-11-15 21:40:15 +1300

    fix: fix incorrect procedures #53 (#17)

--- nzsectorfiles/airspaceboundary.sql
+++ nzsectorfiles/airspaceboundary.sql
@@ -1,8 +1,8 @@
--- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
 -- Host: localhost    Database: nzsectorfiles
 -- ------------------------------------------------------
--- Server version	10.4.22-MariaDB
+-- Server version	10.4.32-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -26,7 +26,7 @@ CREATE TABLE `airspaceboundary` (
   `id_airspace` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_airspace` (`id_airspace`)
-) ENGINE=InnoDB AUTO_INCREMENT=342645 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=342645 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
