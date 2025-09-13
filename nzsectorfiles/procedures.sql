55cc39d1a261ebbb72cee6a589dc46ef8e948944
Richard Bywater <richard.bywater@vatnz.net>
2025-09-13 13:33:19 +1200

    Cycle 2510 Procedure Changes

--- nzsectorfiles/procedures.sql
+++ nzsectorfiles/procedures.sql
@@ -35,7 +35,7 @@ CREATE TABLE `procedures` (
   `sctfilename` varchar(45) NOT NULL,
   `colour` varchar(45) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4535 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4540 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
